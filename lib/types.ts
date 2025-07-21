c260e0d158a449adae258316f9df9a4ebc2a022a
Joksa132 <nikolajoksimovic419@gmail.com>
2025-07-21 14:13:10 +0000

    create types

---
+++ lib/types.ts
@@ -0,0 +1,41 @@
+export type Category = {
+  id: string;
+  created_at: string;
+  updated_at: string;
+  name: string;
+  description: string | null;
+  url: string;
+};
+
+export type Product = {
+  id: string;
+  created_at: string;
+  updated_at: string;
+  name: string;
+  description: string | null;
+  price: number;
+  discounted_price: number | null;
+  category_id: string | null;
+  available_sizes: string[] | null;
+  features: string[] | null;
+  is_active: boolean;
+  is_featured: boolean;
+  average_rating: number | null;
+  review_count: number | null;
+  gender: string;
+  tags: string[] | null;
+  url_slug: string;
+};
+
+export type ProductVariants = {
+  id: string;
+  created_at: string;
+  updated_at: string;
+  product_id: string;
+  color: string;
+  image_urls: string[] | null;
+};
+
+export interface ProductVariantsDetails extends ProductVariants {
+  products: Product;
+}
